7599bd51b836b890210321ff2345d1dca44129b5
EdwardWoda <edwardwoda05@gmail.com>
2024-11-28 21:28:52 +0700

    detail

--- app/src/main/java/com/example/siatep/DetailActivity.kt
+++ app/src/main/java/com/example/siatep/DetailActivity.kt
@@ -1,5 +1,7 @@
 package com.example.siatep
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -24,5 +26,21 @@ class DetailActivity : AppCompatActivity() {
         binding.tvNama.text = name
         binding.tvKelas.text = kelas
         binding.tvCreated.text = tglAbsen
+
+        animateCardView()
+    }
+    private fun animateCardView() {
+        // Animasi Slide Up
+        val slideUp = ObjectAnimator.ofFloat(binding.main, "translationY", 1000f, 0f)
+        slideUp.duration = 800
+
+        // Animasi Fade In
+        val fadeIn = ObjectAnimator.ofFloat(binding.main, "alpha", 0f, 1f)
+        fadeIn.duration = 800
+
+        // Kombinasi Animasi
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(slideUp, fadeIn)
+        animatorSet.start()
     }
 }
